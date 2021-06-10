4facb749c8d9cf9049b459bf523cfac71f7521d9
Arib Nawal <65786174+arib168@users.noreply.github.com>
2021-06-10 13:26:34 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st 
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
+st.title("Basic Calculator")
+a = st.sidebar.text_input("Enter first number")
+#a = st.sidebar.number_input("Enter first number")
+b = st.slider("Enter second number",0,1000)
+select = st.selectbox("How would you like to do?",("Addition", "Subtraction","Multiplication","Division"))
+if select == "Addition":
+  calc = int(a)+int(b)
+elif select == "Subtraction":
+    calc = int(a)-int(b)
+elif select == "Multiplication":
+    calc = int(a)*int(b)
+elif select == "Division":
+    calc = int(a)/int(b)
+if st.button("CALCULATE"):
+    st.sidebar.title(f"The {select} of {a} and {b} is {calc}")
